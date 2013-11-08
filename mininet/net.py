8aadc19c6292e886dad8b931689834ae2054d892
navaneeth.cp <navaneeth.cp@gmail.com>
2013-11-08 12:31:40 +0530

    Update net.py

--- mininet/net.py
+++ mininet/net.py
@@ -385,54 +385,7 @@ class Mininet( object ):
         info( '\n' )
         print(self.hosts)
         l=len(self.hosts)
-        print(l)
-#        class servThread(threading.Thread):
-#            x=[]
-#            def __init__(self,host_list):
-#                servThread.x=host_list
-#                le=len(servThread.x)
-#                print "number of hosts"
-#                print le
-#                threading.Thread.__init__(self)
-#                self.process=None
-#            def run(self):
-#                print servThread.x[0]
-#                print "running server thread"
-#                print servThread.x[0].cmd("python servnew.py")
-#        class host1Thread(threading.Thread):
-#            y=[]
-#            def __init__(self,host_list):
-#                host1Thread.y=host_list
-#                threading.Thread.__init__(self)
-#                self.process=None
-#            def run(self):
-#                print host1Thread.y[1]
-#                print "running host thread"
-#                print servThread.x[0].cmd("python servnew.py")
-#                for i in range(1,len(host1Thread.y)-1):
-#                    print "running"
-#                    print host1Thread.y[i].cmd('python h1.py')
-#                    time.sleep(1)
-#         class servThread(threading.Thread):
-#            z=[]
-#            def __init__(self,host_list):
-#                masterThread.z=host_list
-#                threading.Thread.__init__(self)
-#                self.process=None
-#            def run(self):
-#                print masterThread.z[len(masterThread.z)-1]
-#                print "running master thread"
-#                print masterThread.z[len(masterThread.z)-1].cmd("python master.py")       
-#                
-#         threadOne=servThread(self.hosts)
-#         threadTwo=host1Thread(self.hosts)
-#         threadThree=masterThread(self.hosts)
-#         threadOne.start()
-#         time.sleep(1)
-#         threadTwo.start()
-#         time.sleep(2)
-#         threadThree.start()
-#         time.sleep(22)
+        print("total number of hosts created=%s",%l)
         def serv():
              print self.hosts[0].cmd('python servnew.py')
         def cli():
@@ -451,9 +404,8 @@ class Mininet( object ):
         time.sleep(1)
         for i in range(1,l-1):
             td[i]=threading.Thread(target=cli)
-            time.sleep(1)
             td[i].start()
-            time.sleep(2)
+            time.sleep(1)
             
         tm.start()
         time.sleep(40)
